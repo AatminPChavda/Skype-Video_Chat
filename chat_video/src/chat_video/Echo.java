dcd94c7d5cc42b2926c56d19b7ea7a731d1c531c
AatminPChavda <aatminchavda@gmail.com>
2020-08-06 16:52:10 +0530

    Echo file updated

--- chat_video/src/chat_video/Echo.java
+++ chat_video/src/chat_video/Echo.java
@@ -9,6 +9,72 @@ package chat_video;
  *
  * @author aatmin
  */
-public class Echo {
-    
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class Echo
+{
+    public static void main(String[] args) throws Exception
+    {
+        ServerSocket serverSocket = new ServerSocket(3000);
+        while(true){Thread echoThread = new Thread(new EchoThread(serverSocket.accept()));
+                    echoThread.start();}
+    }
+}
+
+class EchoThread implements Runnable
+{
+    public static Collection<socket> sockets = new ArrayList<socket>();
+    Socket connection = null;
+    DataInputStream dataIn = null;
+    DataOutputStream dataOut = null;
+
+    public EchoThread(Socket conn) throws Exception
+    {
+        connection = conn;
+        dataIn = new DataInputStream(connection.getInputStream());
+        dataOut = new DataOutputStream(connection.getOutputStream());
+        sockets.add(connection);
+    }
+
+    public void run()
+    {
+        int bytesRead = 0;
+        byte[] inBytes = new byte[1];
+        while(bytesRead != -1)
+        {
+            try{bytesRead = dataIn.read(inBytes, 0, inBytes.length);}catch (IOException e){}
+            if(bytesRead >= 0)
+            {
+                sendToAll(inBytes, bytesRead);
+            }
+        }
+        sockets.remove(connection);
+    }
+
+    public static void sendToAll(byte[] byteArray, int q)
+    {
+        Iterator<socket> sockIt = sockets.iterator();
+        while(sockIt.hasNext())
+        {
+            Socket temp;
+            temp = sockIt.next();
+            // skip if this is the sender
+            if (this.connection == temp)
+            {
+                continue;
+            }
+            DataOutputStream tempOut = null;
+            try
+            {
+                tempOut = new DataOutputStream(temp.getOutputStream());
+            } catch (IOException e1)
+            {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+            try{tempOut.write(byteArray, 0, q);}catch (IOException e){}
+        }
+    }
 }
